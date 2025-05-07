b301e67cdffa24d3280597a7a8f3192c39ea2dda
goblincookies <goblincookiebox@gmail.com>
2025-05-07 09:17:38 -0400

    prob 1 solved

---
+++ prob-01.js
@@ -0,0 +1,13 @@
+// Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
+
+// Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
+
+function sumRange( n ) {
+    if ( n == 0 ) { return 0; }
+    return n + sumRange( n - 1 )
+};
+
+let a = sumRange( 3 );
+let b = sumRange( 4 );
+
+console.log( a, b );
